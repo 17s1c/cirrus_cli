1b404d2fb66ccc9c3d2df59347fd25594e9d3733
zizhenli <zizhenli@hk01.com>
2021-09-14 17:14:03 +0800

    feat: update

--- sample/backend_app/.cirrus/webpack.config.js
+++ sample/backend_app/.cirrus/webpack.config.js
@@ -1,33 +1,31 @@
-const webpack = require('webpack');
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack')
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
-  entry: ['webpack/hot/poll?1000', './src/app.module.ts'],
-  target: 'node',
-  externals: [
-    nodeExternals({
-      whitelist: ['webpack/hot/poll?1000'],
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+    entry: ['webpack/hot/poll?1000', './src/app.module.ts'],
+    target: 'node',
+    externals: [
+        nodeExternals({
+            whitelist: ['webpack/hot/poll?1000'],
+        }),
     ],
-  },
-  mode: "development",
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-  output: {
-    path: path.join(__dirname, '../dist'),
-    filename: 'server.js',
-  },
-};
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    mode: 'production',
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+    output: {
+        path: path.join(__dirname, '../dist'),
+        filename: 'server.js',
+    },
+}
